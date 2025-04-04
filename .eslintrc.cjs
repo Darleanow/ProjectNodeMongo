f654d7f21e78a06542897a4a5a9a213fc88189b1
Enzo Hugonnier <enzohugonn@gmail.com>
2025-04-04 11:03:12 +0200

    chore(Config): Fix all config issues

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    node: true,
+    es2022: true,
+    jest: true
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+  }
+}
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,8 @@
-module.exports = {
+export default {
+  preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
+  transform: {
+    '^.+\\.ts$': ['ts-jest', { useESM: true }]
+  }
 }
