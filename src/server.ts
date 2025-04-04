ea5b104885af4ac6e15b89282f26dbb9d2ea6549
Enzo Hugonnier <enzohugonn@gmail.com>
2025-04-04 09:54:06 +0200

    feat(Project): Make great styles

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,39 @@
+// ------------------ SERVER CLEAN SETUP ------------------
+
 import 'dotenv/config'
-import bodyparser from 'body-parser'
 import express from 'express'
+import bodyParser from 'body-parser'
 import cors from 'cors'
-import initRouter from '@/routes/router'
-import connectDB from '@/database/database'
-import userRoutes from '@/routes/user'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+import connectDB from './database/database.js'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const app = express()
 const port = process.env.PORT || 3000
 
+// View engine
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, '../views'))
+
+
 // Middleware
 app.use(cors())
-app.use(bodyparser.json())
-app.use(bodyparser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, '../public')))
 
-connectDB();
-initRouter(app)
+// Connect DB
+connectDB()
 
-app.use('/api/users', userRoutes)
+// Routes
+app.get('/', (req, res) => res.render('index', { title: 'Accueil' }))
+app.get('/map', (req, res) => res.render('map', { title: 'Carte' }))
 
-app.listen(port, () => { console.log(`Server running on port ${port}`) })
+// Start server
+app.listen(port, () => {
+  console.log(`✅ Server running on http://localhost:${port}`)
+})
