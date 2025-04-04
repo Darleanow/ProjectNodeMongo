f6bc871e81c3eeec30436ac8b29cd4b06f0bef06
Enzo Hugonnier <enzohugonn@gmail.com>
2025-04-04 11:05:18 +0200

    fix(Project): Fix database poor typing

--- src/database/database.ts
+++ src/database/database.ts
@@ -4,13 +4,20 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const monURI = process.env.MONGO_URI
+if (!monURI) {
+  throw new Error('❌ MONGO_URI is not defined in environment variables')
+}
 
 const connectDB = async () => {
   try {
     await connect(monURI)
-    console.log('MongoDB Connected...')
-  } catch (err) {
-    console.error(err.message)
+    console.log('✅ MongoDB Connected')
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      console.error('❌ Mongo connection error:', err.message)
+    } else {
+      console.error('❌ Unknown error during DB connection:', err)
+    }
     process.exit(1)
   }
 }
