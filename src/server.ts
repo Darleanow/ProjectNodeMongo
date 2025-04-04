ff63bf9d5679b5dcf3179ffebe0b79a57a0ecb99
Enzo Hugonnier <enzohugonn@gmail.com>
2025-04-04 11:14:52 +0200

    feat(RelLinks): Use '@' imports

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ import cors from 'cors'
 import path from 'path'
 import { fileURLToPath } from 'url'
 
-import connectDB from './database/database.js'
+import connectDB from '@/database/database.js'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
